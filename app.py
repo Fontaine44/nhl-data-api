13557690a84be02b07d9246626da74aed211c0f9
Raphaël Fontaine <raphael.fontaine@mail.mcgill.ca>
2022-12-08 01:11:49 -0500

    Add export

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import export
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return json.dumps(export.run())
 
 if __name__ == '__main__':
     app.run()
---
+++ export.py
@@ -0,0 +1,65 @@
+import json
+from azure.cosmos import CosmosClient, PartitionKey, ContainerProxy, DatabaseProxy
+
+
+def run():
+    endpoint = "https://nhl-data.documents.azure.com:443/"
+    key = "AZURE_PRIMARY_KEY"
+
+    client = CosmosClient(url=endpoint, credential=key)
+    database = client.get_database_client("nhl-data")
+
+    return query_all_shots(database.get_container_client("mtl-shots"))
+
+
+def drop_data(database: DatabaseProxy):
+    try:
+        database.delete_container("mtl-shots")
+    finally:
+        database.create_container("mtl-shots", partition_key=PartitionKey(path='/id'))
+        container = database.get_container_client("mtl-shots")
+
+        with open("shots.json", "r") as f:
+            data = json.load(f)
+        
+        for entry in data:
+            container.create_item(entry, enable_automatic_id_generation=True)
+
+
+def query_all_shots(container: ContainerProxy):
+    qu = """SELECT 
+            c.playerId,
+            c.playerName,
+            c.eventTypeId,
+            c.secondaryType,
+            c["time"],
+            c.period,
+            c.x,
+            c.y,
+            c.teamId,
+            c.team,
+            c.gameId,
+            c.strength,
+            c.distance
+            FROM c
+            WHERE c.strength=@strength
+            AND c.playerName=@playerName
+            """
+
+    params = [
+        {
+            "name":"@strength",
+            "value":"PP"
+        },
+        {
+            "name":"@playerName",
+            "value":"Cole Caufield"
+        }
+    ]
+
+    items = container.query_items(query=qu, parameters=params, enable_cross_partition_query=True)
+    it = list(items)
+    return it
+
+if __name__ == "__main__":
+    run()
