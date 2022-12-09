fecbe642847fe63f4d1d043db7d63b2a67dd80e9
Raphaël Fontaine <95888901+Fontaine44@users.noreply.github.com>
2022-12-09 17:50:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,5 +18,5 @@ def get_shots():
     shots = fetch_shots(params)
     return jsonify(shots)
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int("5000"), debug=True)
+# if __name__ == '__main__':
+#     app.run(host="0.0.0.0", port=int("5000"), debug=True)
