93bed89697bd5cc3e6f2ce6acd0f868ea87da0dc
Raphaël Fontaine <raphael.fontaine@mail.mcgill.ca>
2022-12-08 23:41:29 -0500

    Add CORS

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from shots import fetch_shots
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/shots', methods=["GET"])
 def get_shots():
