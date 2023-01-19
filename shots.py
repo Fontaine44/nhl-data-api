25bc036833238a87bd07073b19eef4e0a529a034
Raphaël Fontaine <raphael.fontaine@mail.mcgill.ca>
2023-01-18 19:49:47 -0500

    Change shots to new database

--- shots.py
+++ shots.py
@@ -6,24 +6,15 @@ import os
 
 ENDPOINT = "https://nhl-data.documents.azure.com:443/"
 KEY = os.getenv("COSMOS_PK")
+DATABASE_NAME = "nhl-data"
+CONTAINER_NAME = "nhl-shots"
 
 
 def fetch_shots(params_dict: dict):
     query = """SELECT 
-        c.playerId,
-        c.playerName,
-        c.eventTypeId,
-        c.secondaryType,
-        c["time"],
-        c.period,
-        c.x,
-        c.y,
-        c.teamId,
-        c.team,
-        c.gameId,
-        c.strength,
-        c.distance,
-        c.crossRed
+        c.shotType,
+        c.xCordAdjusted,
+        c.yCordAdjusted
         FROM c"""
 
     query_params = []
@@ -40,25 +31,25 @@ def fetch_shots(params_dict: dict):
                 query_params.append(f"c.{key}=@{key}")
 
         query += " WHERE " + " AND ".join(query_params)
-    
-    container = get_cosmos_container("mtl-shots")
-    items = container.query_items(query=query, parameters=parameters, enable_cross_partition_query=True)
-    return list(items)
 
-    
-def get_cosmos_container(name):
-    client = CosmosClient(url=ENDPOINT, credential=KEY)
-    database = client.get_database_client("nhl-data")
-    return database.get_container_client(name)
+    with CosmosClient(url=ENDPOINT, credential=KEY) as client:
+        client = CosmosClient(url=ENDPOINT, credential=KEY)
+        database = client.get_database_client(DATABASE_NAME)
+        container = database.get_container_client(CONTAINER_NAME)
+        results = container.query_items(
+            query=query, parameters=parameters, enable_cross_partition_query=True)
+        shots = [shot for shot in results]
+
+    return shots
 
 
 def kde(shots: list):
+    if len(shots) <= 1:
+        return []
     df = pd.DataFrame(shots)
-    df = df.query("x > 24 and crossRed == True")   # remove shots outside the zone
-    df['y'] *= -1
     x, y = np.mgrid[0:101, -43:43]
     positions = np.vstack([x.ravel(), y.ravel()])
-    values = np.vstack([df['x'], df['y']])
+    values = np.vstack([df['xCordAdjusted'], df['yCordAdjusted']])
     kernel = gaussian_kde(values, bw_method="scott")
-    z = np.rot90(np.reshape(kernel(positions).T, (101,86)))
+    z = np.rot90(np.reshape(kernel(positions).T, (101, 86)))
     return z.tolist()
